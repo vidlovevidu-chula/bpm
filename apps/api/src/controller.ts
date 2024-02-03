ab364368f2296bf468f19598298ded61cfe0d11d
LittleNa1000 <naphanccnp@gmail.com>
2024-02-03 23:16:35 +0700

    feat: init nextjs and create api routes

---
+++ apps/api/src/controller.ts
@@ -0,0 +1,99 @@
+import { Request, Response } from "express"
+
+type BpmRecord = {
+  id: string
+  bpm: number
+  timestamp: Date
+}
+
+const bpmRecordsQueue: BpmRecord[] = []
+
+let currentBpmRecords: BpmRecord[] = []
+
+async function registerBpm(req: Request, res: Response) {
+  const id = req.params.id
+  const bpm = req.query.bpm
+  const timestamp = new Date()
+
+  if (!id || !bpm) {
+    return res.status(400).json({
+      message: "Invalid Input",
+    })
+  }
+
+  bpmRecordsQueue.push({
+    id,
+    bpm: parseInt(bpm as string),
+    timestamp,
+  })
+
+  console.log(`Hardware ID: ${id} BPM: ${bpm} Registered at ${timestamp.toLocaleString()}`)
+
+  res.json({
+    message: `Hardware ID: ${id} BPM: ${bpm} Registered at ${timestamp.toLocaleString()}`,
+    success: true,
+  })
+}
+
+async function getQueue(req: Request, res: Response) {
+  res.json({
+    data: bpmRecordsQueue,
+    success: true,
+  })
+}
+
+async function popBpm(req: Request, res: Response) {
+  const count = req.body.count
+
+  if (typeof count !== "number") {
+    return res.status(400).json({
+      message: "Invalid Input",
+    })
+  }
+
+  currentBpmRecords = []
+  let i = 0
+
+  for (; i < count && bpmRecordsQueue.length > 0; i++) {
+    const record = bpmRecordsQueue.shift()
+
+    if (!record) {
+      break
+    }
+
+    currentBpmRecords.push(record)
+
+    console.log(
+      `Popped Hardware ID: ${record.id} BPM: ${record.bpm} Registered at ${record.timestamp.toLocaleString()}`
+    )
+  }
+
+  res.json({
+    message: `Retrieved ${i} BPM records from the queue.`,
+    success: true,
+  })
+}
+
+async function getSong(req: Request, res: Response) {
+  if (currentBpmRecords.length === 0) {
+    return res.status(400).json({
+      success: true,
+      data: {
+        bpm: [60],
+        url: "insert url here",
+      },
+    })
+  }
+
+  const avgBpm = currentBpmRecords.reduce((acc, cur) => acc + cur.bpm, 0) / currentBpmRecords.length
+
+  res.status(400).json({
+    success: true,
+    data: {
+      bpm: currentBpmRecords.map((record) => record.bpm),
+      url: avgBpm,
+    },
+  })
+}
+
+export { registerBpm, getQueue, popBpm, getSong }
--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,25 @@
-import express from "express";
-const app = express();
+import express, { Request, Response } from "express"
+import { registerBpm, getQueue, popBpm, getSong } from "./controller"
+import bodyParser from "body-parser"
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const app = express()
+
+app.use(bodyParser.json())
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({
+    message: "Server is running",
+  })
+})
+
+app.get("/register/:id", registerBpm)
+
+app.get("/queue", getQueue)
+
+app.get("/display", getSong)
+
+app.post("/retrieve", popBpm)
 
 app.listen(3000, () => {
-  console.log("Listening on port 3000");
-});
+  console.log("Listening on port 3000")
+})
